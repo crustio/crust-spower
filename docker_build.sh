c9874485e39a861ae93578fb7d110ae1f4ac31d2
BrianWu <wuhaixian1984@163.com>
2024-07-12 12:37:05 +0800

    Update docker_build.sh to do the docker tag  right after docker build

--- docker_build.sh
+++ docker_build.sh
@@ -3,3 +3,4 @@
 VERSION=$(grep '"version"' package.json | awk -F'"' '{print $4}')
 
 sudo docker build -t crustio/crust-spower:$VERSION .
+sudo docker tag crustio/crust-spower:$VERSION crustio/crust-spower:latest
